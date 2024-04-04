b82b58d3af6cab1c3061e41331a558539602d994
a8117042 <ogakyrk5296@gmail.com>
2024-04-04 15:48:27 +0900

    pytest実施

--- src/flask1/test_app.py
+++ src/flask1/test_app.py
@@ -14,3 +14,17 @@ def client(app):
 def test_hello(client):
     rv = client.get("/")
     assert "Hello Flask!" == rv.data.decode('utf8')
+
+@pytest.mark.parametrize(
+    [
+        "name",
+    ],
+    [
+        pytest.param("ogawa"),
+        pytest.param("higashi")
+    ],
+)
+def test_name(client, name):
+    rv = client.get(f"/hello/{name}")
+    assert f"Hello {name}" == rv.data.decode('utf8')
+    
