30091930e3505f78e3d20d0765d8f0ee2000e5b8
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-04-05 01:06:14 +0900

    テスト名を日本語化

--- src/flask1/test_app.py
+++ src/flask1/test_app.py
@@ -1,29 +1,36 @@
 import pytest
+
 from flask1 import create_app
+
 
 @pytest.fixture
 def app():
-    app = create_app({'TESTING' : True})
+    app = create_app({"TESTING": True})
 
     return app
+
 
 @pytest.fixture
 def client(app):
     return app.test_client()
 
-def test_hello(client):
+
+def test_helloエンドポイントにアクセスした場合文字列が返る(client):
     rv = client.get("/")
-    assert "Hello Flask!" == rv.data.decode('utf8')
+    assert "Hello Flask!" == rv.data.decode("utf8")
+
 
 @pytest.mark.parametrize(
     [
         "name",
     ],
     [
-        pytest.param("ogawa"),
-        pytest.param("higashi")
+        pytest.param("ogawa", id="name = ogawa"),
+        pytest.param("higashi", id="name = higashi"),
     ],
 )
-def test_name(client, name):
+def test_helloエンドポイントに名前をつけた場合その名前が入った文字列が返る(
+    client, name
+):
     rv = client.get(f"/hello/{name}")
-    assert f"Hello {name}" == rv.data.decode('utf8')
+    assert f"Hello {name}" == rv.data.decode("utf8")
